925e76ac3a847e485adbcfdb6c20cd7c27111564
Takieddine HARBI <takieddineharbi@gamil.com>
2021-10-01 11:20:31 +0200

    add Learning automata

--- src/HyperHeuristics/LearningFunctions.jl
+++ src/HyperHeuristics/LearningFunctions.jl
@@ -22,7 +22,7 @@ function learn!(learning_mechanism::reward_punish_LM, ΔFitness::Float64, curren
         #here sure the negative value is the last updated scores
         lastUpdatedValue = abs(learning_mechanism.scores[LLHIndex])
         learning_mechanism.scores = learning_mechanism.scores .+ lastUpdatedValue
-        println(learning_mechanism.scores)
+        
     end
 end
 ################################################################
@@ -116,50 +116,41 @@ function learn!(learning_mechanism::QLearning_LM, ΔFitness::Float64, currentSta
     end
     learning_mechanism.scores = learning_mechanism.Q_Table[nextState, :]
 end
+"""
+learning automata is learning mechanism implemented in the way that is described on [1].
+after each application of a LLH  all the probabilities are updated with respect to some equations
+it need a response of the envirments, two lambda constants and the nbr of states
+[1]: Misir, M. (2012). Intelligent hyper-heuristics: a tool for solving generic optimisation problems.
 
-#this function will be removed after i adapt the epsilon greedy hh with the learning_mechanism
-function fix_reward_punish(HHstate::HH_State, performances::Array{PerformanceFactors,1}, LLHs)
-    #this function give reward or punishment to the applied LLH
-    reward = 1
-    punish = 1
-    for i in 1:length(performances)
-        index=findall(x->x.method_name == LLHs[i].method_name, HHstate.LLHs)
-        
-        if performances[i].ΔFitness < 0
-            HHstate.scores[index[1]] += reward
+"""
+mutable struct LearningAutomata <:LearningMethod
+    method_name::String
+    scores #current  
+    nbrOfActions::Integer # in the thesis is represented by r variables
+    Β # reward and punish represent Β(t) in the thisis (if there is an improvements so r =1 and punish=0)
+    λ1
+    λ2
+end
+#by default we work with λ1 = 0.001 and λ2 = 0 (linear reward inaction according to the testes 
+# pocessed in the thesis where we've token this method)
+LearningAutomata(;lambda1=0.001, lambda2=0, beta=1) = 
+    LearningAutomata("Learning Automata",ones(length(loadAllLLH()))/ length(loadAllLLH()),
+    length(loadAllLLH()), beta, lambda1, lambda2)
+
+function learn!(learning_mechanism::LearningAutomata, ΔFitness::Float64, currentState, LLHIndex::Integer, nextState)
+    println("begin learning ...")
+    LM = learning_mechanism
+    β = ΔFitness <= 0 ? 1 : 0 
+    for i in 1:length(LM.scores)
+        if i == LLHIndex
+            LM.scores[i] = LM.scores[i] +  LM.λ1 * β * (1 - LM.scores[i]) - 
+                                    LM.λ2 * (1 - β) * LM.scores[i]
         else
-            HHstate.scores[index[1]] -= punish
+            LM.scores[i] = LM.scores[i] -  LM.λ1 * β * LM.scores[i] + 
+                                    LM.λ2 * (1 - β) * (1/(LM.nbrOfActions-1) - LM.scores[i])
         end
     end
+    println("end learning ...")
 end
 
-function SARSA_RL(HHstate::HH_State, performances::Array{PerformanceFactors,1}, LLHs)
-    """ 
-    to model the hyper heuristic as reinforcement learning problem we must precise several point namely
-    the state, the reward signal, the action. 
-    - the reward signal is 1 if there is an improvement 0 otherwise
-    - the action is the Set of LLHs (we choose only one on each iteration)
-    - the state we can categorize the range of the fitness values on n interval see [1] as an example
-    - for the first version of this method I'll consider n=1 it means we have only one state
-    in this reinforcement learning method it is imperatively to use current and next LLH in HHState
-    [1]: Choong, S. S., Wong, L. P., & Lim, C. P. (2018). Automatic design of hyper-heuristic
-         based on reinforcement learning. Information Sciences,
-         436–437, 89–107. https://doi.org/10.1016/j.ins.2018.01.005
-    """
-    reward = 1
-    punish = 0 
-    
-    #canstant for the SARSA mechanism their values have taken from [1]
-    α = 0.9
-    γ = 0.8
-    #here we have only one performance  
-    if performances[1].ΔFitness < 0
-        r = reward
-    else
-        r = punish
-    end
-
-    HHstate.scores[HHstate.currentLLH] = HHstate.scores[HHstate.currentLLH] + α * (r + γ * HHstate.scores[HHstate.nextLLH] - HHstate.scores[HHstate.currentLLH])
-    
-end
 
